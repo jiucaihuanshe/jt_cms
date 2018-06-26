b322a8f21e19a7026677a3fe11c1f3e9959fd95b
jiucaihuanshe <948710286@qq.com>
2018-06-26 16:53:05 +0800

    上传图片

--- jt-manage/src/main/java/com/jt/manage/service/FileUploadServiceImpl.java
+++ jt-manage/src/main/java/com/jt/manage/service/FileUploadServiceImpl.java
@@ -1,7 +1,12 @@
 package com.jt.manage.service;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 
@@ -13,16 +18,20 @@ import com.jt.common.vo.PicUploadResult;
 @Service
 public class FileUploadServiceImpl implements FileUploadService {
 
+	private String dirPath = "C:/tedu/workspace/jt-upload/";	//定义本地的磁盘路径
+	private String url = "http://image.jt.com/";	//定义url访问路径
 	/**
 	 * 编程思路
 	 * 1.获取图片的名称
 	 * 2.截取图片的类型
 	 * 3.判断是否为图片格式jpg|gif|png
 	 * 4.判断是否为恶意程序
-	 * 5.
-	 * 6.
-	 * 7.
-	 * 8.
+	 * 5.定义磁盘路径和url访问路径
+	 * 6.准备以时间为界限的文件夹
+	 * 7.让图片不重名 当前的时间毫秒数+三位随机数
+	 * 8.创建文件夹
+	 * 9.开始写盘操作
+	 * 10.将数据准备好返回给客户端
 	 */
 	@Override
 	public PicUploadResult uploadFile(MultipartFile uploadFile) {
@@ -32,7 +41,7 @@ public class FileUploadServiceImpl implements FileUploadService {
 		//2.获取图片的类型	.jpg
 		String fileType = fileName.substring(fileName.lastIndexOf("."));
 		//3.判断是否为图片格式
-		if(!fileName.matches("^.*(jpg|png|gif)&")){
+		if(!fileName.matches("^.*(jpg|png|gif)$")){
 			//表示不是图片类型
 			result.setError(1);
 			return result;	//如果程序出错，则直接返回对象
@@ -45,9 +54,42 @@ public class FileUploadServiceImpl implements FileUploadService {
 		//4.1通过工具类判断是否为图片
 		try {
 			BufferedImage bufferedImage = ImageIO.read(uploadFile.getInputStream());
+			//4.2获取图片的高度和宽度
+			int height = bufferedImage.getHeight();
+			int width = bufferedImage.getWidth();
+			if(height==0 || width==0){
+				result.setError(1);//表示非法程序
+				return result;
+			}
+			
+			//6.准备时间文件夹并且时间格式yyyy/MM/dd/HH
+			String datePathDir = new SimpleDateFormat("yyyy/MM/dd/HH").format(new Date());
+			//7.准备时间	毫秒值+3位随机数
+			String millis = System.currentTimeMillis()+"";
+			Random random = new Random();
+			int randomNum = random.nextInt(999);
+			String randomPath = millis+randomNum;
+			//8.创建文件夹
+			//C:/tedu/workspace/jt-upload/yyyy/MM/dd/HH
+			String LocalPath = dirPath+datePathDir;
+			File file = new File(LocalPath);
+			if(!file.exists()){
+				file.mkdirs();	//如果文件夹不存在，则创建文件夹
+			}
+			//9.通过工具类实现写盘操作
+			String LocalPathFile = LocalPath+"/"+randomPath+fileName;
+			//文件写盘 xxxx/aaa.jpg
+			uploadFile.transferTo(new File(LocalPathFile));
+			//10.将数据准备好返回给客户端
+			result.setHeight(height+"");
+			result.setWidth(width+"");
+			//代表虚拟的全路径
+			String urlPath =url+datePathDir+"/"+randomPath+fileName;
+			result.setUrl(urlPath);
+			return result;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return result;
+		return null;
 	}
 }
